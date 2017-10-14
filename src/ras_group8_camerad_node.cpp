2550fede9cfc5a4d9210f86c4e4ff0bbf7c4810b
Harry Potter <potter@hogwarts.uk>
2017-10-14 22:09:16 +0200

    changeed the depth node so it would not crash

--- src/ras_group8_camerad_node.cpp
+++ src/ras_group8_camerad_node.cpp
@@ -11,7 +11,8 @@
 #include "std_msgs/MultiArrayLayout.h"
 #include "std_msgs/MultiArrayDimension.h"
 #include "std_msgs/Int32MultiArray.h"
-//#include "geometry_msgs/Vector2.h"
+#include "geometry_msgs/PointStamped.h"
+#include "visualization_msgs/Marker.h"//#include "geometry_msgs/Vector2.h"
 
 //try:
 //#include <sensor_msgs/ChannelFloat32>
@@ -31,7 +32,8 @@ class ImageConverter
 
   ros::Subscriber gettarget_;
   //ros::Publisher Target_pub_;
-  ros::Publisher pub_center;
+  ros::Publisher pub_target;
+  ros::Publisher pub_view;
   //ros::Vector2 target;
   cv::Point2i center;
 
@@ -52,7 +54,8 @@ public:
     //   &ImageConverter::depthCb, this);
     image_pub_ = it_.advertise("/image_converter/depth_video",1);
 
-    //pub_center = nh_.advertise<std_msgs::Int32MultiArray>("center_of_object", 1);
+    pub_target = nh_.advertise<geometry_msgs::PointStamped>("/target/camera_frame", 10);
+    pub_view = nh_.advertise<visualization_msgs::Marker>("/target/view", 10);
     //Target_pub_= nh_.advertise<geometry_msgs::Int32MultiArray>("center_of_object", 1);
 
     cv::namedWindow(OPENCV_WINDOW);
@@ -89,6 +92,52 @@ public:
 
   }
 
+  void publish_location(float d, float u, float v)
+  {
+    float alpha=622.4961970701949;
+    float s= 0.0;
+    float u0= 332.6030279162762;
+    float beta=623.3265166773533;
+    float v0=241.27796239580363;
+    float z=d;
+    float y=((v-v0)*z)/beta;
+    float x=(z*(u-u0)-s*y)/alpha;
+    geometry_msgs::PointStamped msg;
+    msg.header.frame_id="/camera_depth_optical_frame";
+    msg.header.stamp=ros::Time::now();
+    msg.point.x=x/1000;
+    msg.point.y=y/1000;
+    msg.point.z=z/1000;
+    pub_target.publish(msg);
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "/camera_depth_optical_frame";
+    marker.header.stamp    = ros::Time::now();
+    marker.ns = "obj";
+    marker.id = 0;
+    marker.type = 2;
+    marker.action = 0;
+    marker.pose.position.x=x/1000;
+    marker.pose.position.y=y/1000;
+    marker.pose.position.z=z/1000;
+    marker.pose.orientation.x = 0;
+    marker.pose.orientation.y = 0;
+    marker.pose.orientation.z = 0;
+    marker.pose.orientation.w = 1.0;
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
+
+    marker.color.r = 0.0;
+    marker.color.g = 1.0;
+    marker.color.b = 0.0;
+    marker.color.a = 1.0;
+
+    marker.lifetime.sec=0;
+
+    pub_view.publish(marker);
+
+  }
+
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
     ROS_INFO("go to image");
@@ -103,34 +152,37 @@ public:
       return;
     }
 
-    // Draw an example circle on the video stream
-    // if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-    //   cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
 
     cv::Mat DepthImage=cv_ptr->image;
     
     //cv::Mat croppedDepthImage = DepthImage(Range(10,460), Range(0,600));
 
-    cv::Rect myROI(37, 60, 481, 362);
+
+    int colmin = 0;
+    int rowmin = 60;
+    int colmax = 460;
+    int rowmax = 433;
+
+    // x, y, height, width !!!
+    cv::Rect myROI(colmin, rowmin, colmax-colmin, rowmax-rowmin);
 
     // Crop the full image to that image contained by the rectangle myROI
     // Note that this doesn't copy the data
     cv::Mat croppedDepthImage = DepthImage(myROI);
+    ROS_INFO("1");
 
 
 
 
-    std_msgs::Int32MultiArray array;
+
+
 
     int rows = croppedDepthImage.rows;
     int cols = croppedDepthImage.cols;
 
-    ROS_INFO("size of image %d, %d",rows,cols);
+    //ROS_INFO("size of image %d, %d",rows,cols);
     
 
-
-
-    //pub_center.publish(array);
     cv::Scalar red(255,255,255);
 
 
@@ -146,9 +198,36 @@ public:
 
     //cv::Point2i pt(center.x+cols/2,center.y+rows/2);
     //cv::Point2i pt(center.x*cols/640+cols/2,center.y*rows/480+rows/2);
+    if (center.x!=0, center.y!=0)
+    {
+      ROS_INFO("2");
     cv::Point2i pt(center.x*cols/640,center.y*rows/480);
+    ROS_INFO("3");
+
+    ROS_INFO("x = %i, y= %i", pt.x, pt.y);
+    ROS_INFO("rows = %i, cols= %i", rows, cols);
+
+
+    //The following if statement is needed so that the program doesn't crash in some situations
+    if ((pt.x<cols-50) && (pt.y<rows)){
+
+        //std::cout << typeid(pt.x).name() << '\n';
+    ROS_INFO("4");
+    float depth_value=croppedDepthImage.at<int>(pt.x,pt.y);
+
+    ROS_INFO("value %f",depth_value);
+    publish_location(depth_value, pt.x/cols*640, pt.y/rows*480);
     
-    cv::circle(croppedDepthImage, pt, 45, red, 3);
+    int circle_radius = 80;
+
+    cv::Point2i pt2(0,0);
+    cv::Point2i pt3(cols, rows);
+
+    cv::circle(croppedDepthImage, pt2, 20, red, 3);
+    cv::circle(croppedDepthImage, pt3, 20, red, 3);
+    cv::circle(croppedDepthImage, pt, circle_radius, red, 3);
+    }
+  }
 
 
     //ROS_INFO("depth %f",croppedDepthImage(center.x*cols/640+cols/2,center.y*rows/480+rows/2));
@@ -158,9 +237,7 @@ public:
     cv::imshow(OPENCV_WINDOW, croppedDepthImage);
     //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
     cv::waitKey(3);    // Output modified video stream
-
-
-    image_pub_.publish(cv_ptr->toImageMsg());
+    //image_pub_.publish(cv_ptr->toImageMsg());
 
 
 
